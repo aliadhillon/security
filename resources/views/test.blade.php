0a0e13146b7abb223caef54fa3db5f21e057337a
Ali A. Dhillon <ali.dhillon102@gmail.com>
2019-08-17 23:23:33 +0500

    added test route, controller and view

---
+++ resources/views/test.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h2>Test</h2>
+        <p>This is just a test</p>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,10 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/test', 'TestController')->name('test');
